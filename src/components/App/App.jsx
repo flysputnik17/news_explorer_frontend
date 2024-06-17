d52045ac47e90f0b03b3af53d035cb8ce14ddb2c
KODKOD177 <pavel2012155@gmail.com>
2024-06-17 23:03:02 +0300

    start working on the header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 import "./App.css";
 import Header from "../Header/Header";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
   return (
     <div className="page">
-      <div className="page__content">
-        <Header />
+      <div className="page__upper-content">
+        <Header isLoggedIn={isLoggedIn} />
+        {/* <Routes>
+          <Route exact path="/"></Route>
+        </Routes> */}
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,49 @@
-function Header() {
+// import { Link } from "react-router-dom";
+import "./Header.css";
+
+function Header({ isLoggedIn }) {
   return (
-    <header>
-      <div>
-        <h1>test</h1>
+    <header className="header">
+      <div className="header__content">
+        {/* <Link to="/">
+          
+        </Link> */}
+        <h1 className="header__title">NewsExplorer</h1>
+
+        {isLoggedIn ? (
+          <>
+            <div className="header__buttons">
+              <button className="header__buttons-homeButton" type="button">
+                Home
+              </button>
+              <button className="header__buttons-articals" type="button">
+                Saved articles
+              </button>
+              <button className="header__buttons-elise" type="button">
+                Elise
+              </button>
+              <img
+                src="../../images/logout.svg"
+                className="header__buttons-elise-image"
+                alt="img"
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="header__buttons">
+              <button className="header__buttons-homeButton" type="button">
+                Home
+              </button>
+              <button
+                className="header__unsigned-buttons-signInButton"
+                type="button"
+              >
+                Sign in
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </header>
   );
