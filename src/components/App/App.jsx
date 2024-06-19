8feff0378e3c1c8fd89672c95b7b4a0021eb61d0
KODKOD177 <pavel2012155@gmail.com>
2024-06-19 23:23:14 +0300

    route mid work save

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,14 +7,22 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div className="page">
       <div className="page__upper-content">
         <Header isLoggedIn={isLoggedIn} />
-        <Main isLoggedIn={isLoggedIn} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Main isLoggedIn={isLoggedIn} />}
+          ></Route>
+          {/* <Route exact path="/profile" element={}></Route> */}
+        </Routes>
       </div>
+
       <About />
       <Footer />
     </div>
