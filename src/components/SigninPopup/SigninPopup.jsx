d83c6ea863bb73f5966bd48742e02b22d6b76de7
KODKOD177 <pavel2012155@gmail.com>
2024-06-24 19:55:31 +0300

    SigninPopUP button fix

--- src/components/SigninPopup/SigninPopup.jsx
+++ src/components/SigninPopup/SigninPopup.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
 import PopupWithForm from "../PopupWithForm/PopupWithForm";
 
 const SigninPopup = ({ isOpen, onClose, handleSignupButton }) => {
+  const [buttonStyle, setButtonStyle] = useState("SignUp__button-disabled");
+  const [buttonText, setButtonText] = useState("Disabled");
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signinButton = document.getElementById("signinButton");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  useEffect(() => {
+    if (email.length && password.length > 0) {
+      setButtonStyle("SignUp__button");
+      setButtonText("Sign in");
+      signinButton.disabled = false;
+    } else {
+      return;
+    }
+  }, [email, password]);
+
   return (
     <PopupWithForm titleText="Sign In" isOpen={isOpen} onClose={onClose}>
       <label htmlFor="email" className="modal__label">
@@ -10,7 +35,9 @@ const SigninPopup = ({ isOpen, onClose, handleSignupButton }) => {
           name="email"
           className="modal__input"
           id="email"
+          value={email}
           placeholder="Enter email"
+          onChange={handleEmailChange}
           required
         />
       </label>
@@ -22,16 +49,18 @@ const SigninPopup = ({ isOpen, onClose, handleSignupButton }) => {
           name="Enter password"
           type="text"
           placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
           required
         />
       </label>
 
-      <button type="submit" className="SignUp__button">
-        Sign in
+      <button id="signinButton" disabled type="submit" className={buttonStyle}>
+        {buttonText}
       </button>
       <button
         type="button"
-        className="Or-Login__button"
+        className="Or-Sign-Up__button"
         onClick={handleSignupButton}
       >
         or Sign up
