912f86ce4a2204b2a701c6c216b5629f559b15d1
KODKOD177 <pavel2012155@gmail.com>
2024-07-21 21:33:26 +0300

    removing unused parmas

--- src/components/NewsCardList/NewsCardList.jsx
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -67,7 +67,6 @@ const NewsCardList = ({
                     news={news}
                     handleDeleteArticle={handleDeleteArticle}
                     keywords={keywords}
-                    savedArticles={savedArticles}
                   />
                 ))}
               </div>
